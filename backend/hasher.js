788365bb6a975182972e494abe3e9691c8d59a0f
David (Favour) Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2024-06-24 19:58:18 +0100

    Add backend to repository

---
+++ backend/hasher.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs');
+
+async function hashPassword() {
+    const password = 'David$mith1';
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log('Hashed Password:', hashedPassword);
+}
+
+hashPassword();
